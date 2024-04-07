ec513c194906c98592d8431ca8a35b8e2a8bb010
sandhaligupta18 <117626843+sandhaligupta18@users.noreply.github.com>
2024-04-07 22:30:38 +0530

    crud using the code first approach

---
+++ CodeFirstApproachCrud/CodeFirstApproachCrud/Controllers/DemoController.cs
@@ -0,0 +1,68 @@
+﻿using CodeFirstApproachCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirstApproachCrud.Controllers
+{
+    public class DemoController : Controller
+    {
+        EmpDataContext objDataContext = new EmpDataContext();
+        // GET: Demo  
+        public ActionResult EmpDetails()
+        {
+
+            return View(objDataContext.employees.ToList());
+        }
+        [HttpGet]
+        public ActionResult create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult create(Employee objEmp)
+        {
+            objDataContext.employees.Add(objEmp);
+            objDataContext.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Details(string id)
+        {
+            int empId = Convert.ToInt32(id);
+            var emp = objDataContext.employees.Find(empId);
+            return View(emp);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            int empId = Convert.ToInt32(id);
+            var emp = objDataContext.employees.Find(empId);
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee objEmp)
+        {
+            var data = objDataContext.employees.Find(objEmp.EmpId);
+            if (data != null)
+            {
+                data.Name = objEmp.Name;
+                data.Address = objEmp.Address;
+                data.Email = objEmp.Email;
+                data.MobileNo = objEmp.MobileNo;
+            }
+            objDataContext.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Delete(string id)
+        {
+            int empId = Convert.ToInt32(id);
+            var emp = objDataContext.employees.Find(empId);
+            return View(emp);
+        }
+    }
+}
---
+++ CodeFirstApproachCrud/CodeFirstApproachCrud/Models/EmpDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CodeFirstApproachCrud.Models
+{
+    public class EmpDataContext: DbContext
+    {
+        public EmpDataContext()
+           : base("name=MySqlConnection")
+        {
+        }
+        public DbSet<Employee> employees { get; set; }
+    }
+}
